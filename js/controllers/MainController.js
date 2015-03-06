e0ba5d6ff6a5fd3eadc956646a86dc0d14512917
David Mandel <MandelDL@gmail.com>
2015-03-06 15:32:34 -0500

    added another object in the program controller and referenced it with program-listing directive

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -8,4 +8,13 @@ app.controller('MainController', ['$scope', function($scope) {
 	  description: "Two years after his reported Reichenbach Fall demise, Sherlock, who has been cleared of all fraud charges against him, returns with Mycroft's help to a London under threat of terrorist attack. John has moved on and has a girlfriend, Mary Morstan. Sherlock enlists Molly to assist him, but when John is kidnapped by unknown assailants and is rescued by Sherlock and Mary, John returns to help find the terrorists and an underground plot to blow up the Houses of Parliament during an all night sitting on Guy Fawkes Night.",
 	  datetime: new Date(2014, 11, 31, 21, 00, 00, 00)
   };
+  $scope.arrested = {
+  	series: "Arrested Development",
+	  series_img: "img/arrested.jpg",
+	  genre: "Comedy",
+	  season: 2,
+	  episode: "The One Where Michael Leaves",
+	  description: "George Sr. is at large, making Michael next in line to be prosecuted for the Bluth Company crimes. Meanwhile, Michael is vexed to find that the family is fine without him, and police mistake Oscar for George Sr. and arrest him.",
+	  datetime: new Date(2004, 11, 07, 21, 30, 00, 00)
+  }
 }]);
